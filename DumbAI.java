2855179be580b497b36358c3bd60e6ac81dd85c6
gitSANTAplz <91291573+gitSANTAplz@users.noreply.github.com>
2023-01-17 19:59:17 -0330

    template files from class

---
+++ DumbAI.java
@@ -0,0 +1,32 @@
+/**
+ * Realization of AI interface using simplistic random placement strategy.
+ */
+
+import java.util.Random;
+
+public class DumbAI implements AI {
+    
+    private Random random = new Random();
+
+    /*
+     * TBD: Create additional private members if useful.
+     */
+    
+    /**
+     * Construct a DumbAI.
+     * 
+     * @param aiIsX Indicates whether the AI player's piece is
+     *              the 'X'.
+     */
+    public DumbAI(boolean aiIsX) {
+        /*
+         * TBD
+         */
+    }
+
+    public Move chooseMove(Board board) {
+        /*
+         * TBD
+         */
+    }
+}
